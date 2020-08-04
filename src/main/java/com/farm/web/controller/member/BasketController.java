e29116cfe8b57c33adad97996692d94cb660643a
yuno <hanganda23@naver.com>
2020-08-04 12:12:41 +0900

    0804 pricipal,securityConfig update

--- src/main/java/com/farm/web/controller/member/BasketController.java
+++ src/main/java/com/farm/web/controller/member/BasketController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.farm.web.config.MyUserDetails;
 import com.farm.web.entity.BasketPayView;
 import com.farm.web.entity.BasketView;
 import com.farm.web.entity.OrderSheet;
